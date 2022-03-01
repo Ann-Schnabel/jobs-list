3970c3e1c16f5ecaf1fa429f9e45dcb0536b86b5
Ann Schnabel <anntschnabel@gmail.com>
2022-03-01 01:45:10 -0500

    router setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { AddEdit, JobsList } from "./pages";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<JobsList />} />
+        <Route path="edit" element={<AddEdit />} />
+        <Route path="add" element={<AddEdit />} />
+        <Route path="*" element="404: Not Found" />
+      </Routes>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
